9478f317895286873e058fe350541329e6819142
edatrix <lespath@gmail.com>
2014-01-07 13:22:37 -0700

    initial setup with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Runline::Application.config.secret_key_base = '6855cee6fe19bd7959efd1a049b380fe3ecf5ed868904501bc4e41e743803155a438bc7869b86817e827db1cfe168798a66df8e2205d25901b9467e2ac25dfc0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Runline::Application.config.session_store :cookie_store, key: '_runline_session'
