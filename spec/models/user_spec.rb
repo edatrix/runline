e0715b7f67341ecb33d39dc0219e512f8fe70a41
edatrix <lespath@gmail.com>
2014-01-07 14:21:10 -0700

    created user table with username and email validation tests passing

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe User do
+
+  it "should require a username" do
+    User.new(:username => "").should_not be_valid
+  end
+
+  it "should require an email" do
+    User.new(:username => "Bob", :email => "").should_not be_valid
+  end
+
+end
