0f3fab8980c4827a7e0a2a806afa66902483bf84
Billy <navyosu@gmail.com>
2014-01-09 10:48:47 -0700

    Added validation for friend id on friendships

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -7,4 +7,15 @@ describe Friendship do
     friendship.update(:user_id => nil)
     expect(friendship).not_to be_valid
   end
+
+  it "must have a friend associated with it" do
+    user = FactoryGirl.create(:user)
+    friendship = FactoryGirl.create(:friendship)
+    friendship.update(:friend_id => nil)
+    expect(friendship).not_to be_valid
+  end
+
+  xit "has a friend that is also a user" do 
+    expect(friend.class).to eq(User)
+  end
 end
