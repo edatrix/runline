60011f639bb102b6ac80bb7d82204d8721f96689
Billy <navyosu@gmail.com>
2014-01-09 10:34:05 -0700

    Added validation for user_id on friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,5 @@
 class Friendship < ActiveRecord::Base
+
+  validates :user_id, presence: true
+  # validates :friend_id, presence: true
 end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Friendship do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "must have a user associated with it" do
+    user = FactoryGirl.create(:user)
+    friendship = FactoryGirl.create(:friendship)
+    friendship.update(:user_id => nil)
+    expect(friendship).not_to be_valid
+  end
 end
