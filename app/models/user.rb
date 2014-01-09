744992b6e97b60466f7562a93aeb41fb44dca090
edatrix <lespath@gmail.com>
2014-01-09 13:33:24 -0700

    addes seed data User and three runs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :runs
 
   def self.find_or_create_by_auth(auth)
-    find_or_create_by_provider_and_uid(auth["provider"], 
+    find_or_create_by_provider_and_uid(auth["provider"],
                                        auth["uid"],
                                        username: auth["info"]["name"],
                                        email: auth["info"]["email"])
