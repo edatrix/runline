bfcd33c55cb5f3a3d0929c8ccf7b58210dadfec3
Billy <navyosu@gmail.com>
2014-01-09 10:25:53 -0700

    Added friendships table with user_id and friend_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140108230106) do
+ActiveRecord::Schema.define(version: 20140109172349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "friendships", ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", using: :btree
 
   create_table "runs", force: true do |t|
     t.string   "name"
