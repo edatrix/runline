545fd2b5a4c7021e4c135a28df6457567dcbb88a
Billy <navyosu@gmail.com>
2014-01-09 10:56:09 -0700

    Added belongs to a friend relationship for user and designated class as User to ensure a friend is actually a User

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -5,6 +5,7 @@ describe Friendship do
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:friend_id) }
   it { should belong_to(:user) }
+  it { should belong_to(:friend) }
 
   xit "has a friend that is also a user" do 
     expect(friend.class).to eq(User)
