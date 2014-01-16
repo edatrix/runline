3a468fe39d8dc3b30abf6118dc69386243bcb9c2
Billy <navyosu@gmail.com>
2014-01-16 09:32:50 -0700

    Added tests for rejecting and approving friends

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -6,6 +6,7 @@ describe Friendship do
     @user1 = FactoryGirl.create(:user, username: "user1")
     @user2 = FactoryGirl.create(:user, username: "user2")
     @user3 = FactoryGirl.create(:user, username: "user3")
+    @user4 = FactoryGirl.create(:user, username: "user4")
     Friendship.create(user_id: 1, friend_id: 2, status: "approved")
     Friendship.create(user_id: 1, friend_id: 3, status: "approved")
     Friendship.create(user_id: 2, friend_id: 3, status: "approved")
@@ -34,12 +35,21 @@ describe Friendship do
     expect(@user1.total_approved_friends.count).to eq(1)
   end
 
-  xit "can approve a friend" do 
-
+  it "can approve a friend" do 
+    expect(@user1.total_approved_friends.count).to eq(2)
+    friendship = Friendship.create(user_id: @user4.id, friend_id: @user1.id, status: "pending")
+    friendship.approve
+    expect(@user1.total_approved_friends.count).to eq(3)
   end
 
-  xit "can reject a friend" do 
-
+  it "can reject a friend" do 
+    expect(@user1.total_pending_friends.count).to eq(0)
+    friendship = Friendship.create(user_id: @user4.id, friend_id: @user1.id, status: "pending")
+    expect(@user1.total_pending_friends.count).to eq(1)
+    expect(Friendship.count).to eq(4)
+    friendship.reject
+    expect(Friendship.count).to eq(3)
+    expect(@user1.total_pending_friends.count).to eq(0)
   end
 
   context "changing friendship status" do
