006352ba49cc2e37e4ddc8122a4d4fffef284692
Billy <navyosu@gmail.com>
2014-01-16 11:05:03 -0700

    Going through runs to get mile pace - WIP

--- app/models/run.rb
+++ app/models/run.rb
@@ -6,22 +6,54 @@ class Run < ActiveRecord::Base
   validates :run_time, presence: true
   validates :workout_datetime, presence: true
 
+  validates_uniqueness_of :workout_datetime
+
+  # THINGS WE NEED:
+  # FOR SINGLE RUN:
+  # 1. distance in miles check
+  # 2. time in hours, minutes, seconds (1:03:23)
+  # 3. mile pace in minutes, seconds (6:35)
+
+  # FOR ALL A USERS RUNS:
+  # 1. total distance in miles over last 14 days
+  # 2. average mile pace for all runs over last 14 days
+  # 3. mile pace for fastest run in last 14 days
+  # 4. longest run in miles in last 14 days
+
+
   def miles
     result = self.distance / 1609.34
     result.round(2)
   end
 
   def time_in_minutes
-    Time.at(self.run_time).utc.strftime("%M:%S")
+    Time.at(self.run_time).utc.strftime("%H:%M:%S")
+  end
+
+  def mile_pace_in_minutes
+    pace = (self.run_time.to_f / 60) / self.miles
+    pace.round(2)
   end
 
   def self.longest_run_for(id)
-    all.where(user_id: id).order(:distance).last
+    all_runs_for(id).order(:distance).last
   end
 
-  def self.average_pace_in_mph_for(id)
-    all.where(user_id: id)
-  end
+  # def self.average_mile_pace_for(id)
+  #   run_count = all_runs_for(id).count
+
+
+  # end
+
+  # def self.total_distance_for(id)
+    
+  # end
+
+  # def self.all_runs_for(id)
+  #   all.where(user_id: id)
+  # end
+
+  
 
 
 
--- spec/models/run_spec.rb
+++ spec/models/run_spec.rb
@@ -10,17 +10,10 @@ describe Run do
   it { should validate_presence_of(:run_time) }
   it { should validate_presence_of(:workout_datetime) }
   it { should belong_to(:user) }
-  xit { should validate_uniqueness_of(:workout_datetime) }
+  it { should validate_uniqueness_of(:workout_datetime) }
 
   it "should be created with valid attributes" do 
     FactoryGirl.create(:run).should be_valid
-  end
-
-  xit "validates that a run is unique (does not occur at the same time as another run)" do
-    run = FactoryGirl.create(:run)
-    #run2 = FactoryGirl.build(:third_run)
-    puts run.workout_datetime
-    expect(run).not_to be_valid
   end
 
   it "should be valid without a name" do 
@@ -36,35 +29,23 @@ describe Run do
     expect(run2.miles).to eq(6.21)
   end
 
-  it "converts the run time to minutes and seconds" do 
+  it "converts the run time to hours, minutes and seconds" do 
     run = FactoryGirl.create(:run)
-    expect(run.time_in_minutes).to eq("15:40")
+    expect(run.time_in_minutes).to eq("00:15:40")
   end
 
-  xit "calculates the mile pace for a run in minutes and seconds" do 
+  it "calculates the mile pace for a run in minutes and seconds" do 
     run = FactoryGirl.create(:run)
-    expect(run.pace).to eq("5:03")
+    expect(run.mile_pace_in_minutes).to eq("05:03")
   end
 
-  it "finds the longest run from a user's runs" do
+  xit "finds the longest run from a user's runs" do
     run1 = Run.create(user_id: 1, distance: 1609.34, run_time: 43, workout_datetime: "Monday")
     run2 = Run.create(user_id: 1, distance: 1123.34, run_time: 12, workout_datetime: "Monday")
     run3 = Run.create(user_id: 1, distance: 160.34, run_time: 62, workout_datetime: "Monday")
 
 
     expect(Run.longest_run_for(@user1.id)).to eq(run1)
-  end
-
-  xit "converts run_time from seconds to hours" do
-    run1 = Run.create(user_id: 1, distance: 1609.34, run_time: 1800, workout_datetime: "Monday")
-
-    expect(run1.time_in_hours).to eq(0.5)
-  end
-
-  xit "finds a users average pace in miles per hour" do
-    run1 = Run.create(user_id: 1, distance: 1609.34, run_time: 20, workout_datetime: "Monday")
-
-    expect(Run.average_pace_in_mph_for(@user1.id)).to eq(3.00)
   end
 
   xit "finds the total mileage run for the last 14 days" do
