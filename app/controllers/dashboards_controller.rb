0c755d496bfe6b6de4a916ab6fdd090c944a7a2a
Billy <navyosu@gmail.com>
2014-01-16 21:25:43 -0700

    Trying to get user hash from mmf

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,17 @@ class DashboardsController < ApplicationController
 
   def index
     @user = current_user
+
+    store = MapMyFitness::WorkoutStore.new(current_user.token)
+    runs = store.workouts_by(current_user.uid)
+    runs.each do |run|
+      Run.new(
+              user_id: User.find_by_uid(current_user.uid).id,
+              distance: 900,
+              name: run.name,
+              run_time: run.duration,
+              workout_datetime: run.starts_at
+             )
   end
 
   def show
