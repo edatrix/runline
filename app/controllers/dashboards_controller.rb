bd87eb50d202383ee379b83c0151d2b7dd6f09f4
Billy <navyosu@gmail.com>
2014-01-16 22:42:06 -0700

    Removed api request from dashboards controller and moved to sessions controller for testing

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,21 +1,7 @@
 class DashboardsController < ApplicationController
 
-  def index
-    @user = current_user
-
-    store = MapMyFitness::WorkoutStore.new(current_user.token)
-    runs = store.workouts_by(current_user.uid)
-    runs.each do |run|
-      Run.new(
-              user_id: User.find_by_uid(current_user.uid).id,
-              distance: 900,
-              name: run.name,
-              run_time: run.duration,
-              workout_datetime: run.starts_at
-             )
-  end
-
   def show
     @user = current_user
   end
+  
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,19 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_by_auth(user_data)
     session[:user_id] = @user.id
     @user.save!
+
+    # store = MapMyFitness::WorkoutStore.new(current_user.token)
+    # runs = store.workouts_by(current_user.uid)
+    # runs.each do |run|
+    #   Run.new(
+    #           user_id: User.find_by_uid(current_user.uid).id,
+    #           distance: 900,
+    #           name: run.name,
+    #           run_time: run.duration,
+    #           workout_datetime: run.starts_at
+    #          )
+    # end
+    
     redirect_to dashboard_path, notice: "You are logged in!"
   end
 
