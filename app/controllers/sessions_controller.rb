c11bcc693588789da03e4f49b3226ab31f897d90
Billy <navyosu@gmail.com>
2014-01-18 18:53:53 -0700

    Changed find_or_create_by method to reflect correct Rails 4 way

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,13 @@ class SessionsController < ApplicationController
     runs = store.workouts_for_last_14_days_by(current_user.uid)
     # puts runs.inspect
     runs.each do |run|
-      Run.find_or_create_by_mmf_identifier(run.id,
-              user_id: User.find_by_uid(current_user.uid).id,
-              name: run.name,
-              distance: run.distance,
-              run_time: run.duration,
-              workout_datetime: run.started_at
-             )
+      Run.where(:mmf_identifier => run.id).first_or_create(
+                            user_id: User.find_by_uid(current_user.uid).id,
+                            name: run.name,
+                            distance: run.distance,
+                            run_time: run.duration,
+                            workout_datetime: run.started_at 
+                           )
     end
     
     redirect_to dashboard_path, notice: "You are logged in!"
