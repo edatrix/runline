06c55f2d25cad89892822f5a2494db599700b1f1
Billy <navyosu@gmail.com>
2014-01-20 15:20:09 -0700

    Removed old commented out code in tests that won't be used

--- spec/features/user_creation_spec.rb
+++ spec/features/user_creation_spec.rb
@@ -11,12 +11,6 @@ describe "User Signup" do
     click_on("Login with MapMyFitness")
     expect(page).to have_content "Aggregate Stats 14 Days"
   end
-
-  # it "clicks the login button" do
-  #   page.visit root_path 
-  #   click_link "Login with Facebook"
-  #   expect(current_path).to eq("http://www.facebook.com")
-  # end
-
+  
 end
 
