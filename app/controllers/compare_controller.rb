5f0602b9f675df114138bc6977b8d3ab9f56e127
Billy <navyosu@gmail.com>
2014-01-20 18:32:01 -0700

    Differentiate comparison if no runs exist for user or friend

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -1,12 +1,25 @@
 class CompareController < ApplicationController
 
   def show
-    @friend = current_user.total_approved_friends.find(params[:id]).first
-    @my_runs = current_user.runs
-    @friend_runs = @friend.runs
-    @diff_miles = (current_user.total_distance_in_miles - @friend.total_distance_in_miles).round(2)
-    @diff_pace = (current_user.pace - @friend.pace)
-    @diff_longest_run = (current_user.longest_run.miles - @friend.longest_run.miles).round(2)
+    @friend = find_current_friend(params[:id])
+    unless current_user.runs.empty? || @friend.runs.empty?
+      @my_runs = current_user.runs
+      @friend_runs = @friend.runs
+      @diff_miles = (current_user.total_distance_in_miles - @friend.total_distance_in_miles).round(2)
+      @diff_pace = (current_user.pace - @friend.pace)
+      @diff_longest_run = (current_user.longest_run.miles - @friend.longest_run.miles).round(2)
+    end
+  end
+
+  def find_current_friend(id)
+    current_friend = []
+    current_user.total_approved_friends.each do |friend|
+      if friend.id == id.to_i
+        current_friend << friend
+      end
+      current_friend
+    end
+    current_friend.first
   end
 
 end
