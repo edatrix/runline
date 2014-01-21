95d326edcca9be3d61159ac81d0cd6c1e30e5187
Billy <navyosu@gmail.com>
2014-01-20 19:20:59 -0700

    Changed job to reflect gem's way of fetching data from api scoped to days

--- app/jobs/fetch_runs_worker.rb
+++ app/jobs/fetch_runs_worker.rb
@@ -3,7 +3,7 @@ class FetchRunsWorker
   def work
     User.where("token IS NOT NULL AND uid IS NOT NULL").each do |user|
       store = MapMyFitness::WorkoutStore.new(user.token)
-      runs = store.workouts_for_last_14_days_by(user.uid)
+      runs = store.workouts_by_user_for_last_days(user.uid, 14)
       runs.each do |run|
         Run.where(:mmf_identifier => run.id).first_or_create(
                               user_id: User.find_by_uid(user.uid).id,
