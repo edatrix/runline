ae163f73f835c2d0e3646f936f67e4e16d777c0b
Luke Martinez <lukemartinez@gmail.com>
2014-01-22 13:19:16 -0700

    reduced duplication in format_for_views method

--- app/models/run.rb
+++ app/models/run.rb
@@ -12,24 +12,11 @@ class Run < ActiveRecord::Base
   end
 
   def time_in_minutes
-    format_seconds_for_views(self.run_time)
+    Formatting.format_seconds_for_views(self.run_time)
   end
 
   def mile_pace_in_minutes
-    format_seconds_for_views((self.run_time / self.miles))
-  end
-
-  def format_seconds_for_views(total_seconds)
-    hours = (total_seconds / 3600).to_i
-    minutes = ((total_seconds / 60) % 60).to_i.to_s
-    seconds = (total_seconds % 60).round.to_s
-    seconds = "0" + seconds if seconds.length == 1
-    if hours >= 1
-      minutes = "0" + minutes if minutes.length == 1
-      "#{hours}:#{minutes}:#{seconds}"
-    else
-      "#{minutes}:#{seconds}"
-    end
+    Formatting.format_seconds_for_views((self.run_time / self.miles))
   end
 
   def format_date
--- app/models/run_stat_calculator.rb
+++ app/models/run_stat_calculator.rb
@@ -9,7 +9,7 @@ class RunStatCalculator
       user.runs.each do |run|
         total_distance += run.miles
       end
-      user.format_seconds_for_views(total_seconds / user.total_distance_in_miles)
+      Formatting.format_seconds_for_views(total_seconds / user.total_distance_in_miles)
     end
 
     def pace_for(user)
@@ -27,10 +27,10 @@ class RunStatCalculator
     def compare_total_average_mile_pace_for(user, friend)
       if user.pace > friend.pace
         diff = user.pace - friend.pace
-        "Your average mile is #{user.format_seconds_for_views(diff)} slower than #{friend.username}'s"
+        "Your average mile is #{Formatting.format_seconds_for_views(diff)} slower than #{friend.username}'s"
       else
         diff = friend.pace - user.pace
-        "Your average mile is #{user.format_seconds_for_views(diff)} faster than #{friend.username}'s"
+        "Your average mile is #{Formatting.format_seconds_for_views(diff)} faster than #{friend.username}'s"
       end
     end
 
