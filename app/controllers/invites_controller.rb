cefe7a6b867a81984d9b16a2eb2b0464fd34f93b
edatrix <lespath@gmail.com>
2014-01-22 10:27:54 -0700

    working on email

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,10 +1,10 @@
 class InvitesController < ApplicationController
 
   def create
-    User.send_friend_request_email(params[:email], current_user.username)
-    
+    User.invite_new_friend_email(params[:email], current_user.username)
+
     flash.notice = "Your request to #{params[:email]} has been sent!"
-    
+
     redirect_to :back
   end
 
