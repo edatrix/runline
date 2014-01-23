1995a4baa563904b9c0acad4f91d9eb5df4d2f62
Billy <navyosu@gmail.com>
2014-01-23 12:46:23 -0700

    Changed flash notices for email update and inviting new friend to proper syntax

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,7 +3,7 @@ class InvitesController < ApplicationController
   def create
     User.invite_new_friend_email(params[:email], current_user.username)
 
-    flash.notice = "Your request to #{params[:email]} has been sent!"
+    flash[:notice] = {:class => "flash", :body => "Your request to #{params[:email]} has been sent!"}
 
     redirect_to :back
   end
